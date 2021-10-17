e6796a942a496d5d75b4a963b04d219b62270fd6
nagendra <gamparaghavendra143>
2021-10-17 15:03:38 +0530

    ok

---
+++ sample1.py
@@ -0,0 +1,15 @@
+import os
+list1 = [8,9,3,4,6,1,10]
+list1.reverse()
+print("the reverse list is:",list1)
+
+list2 = [8,9,3,4,6,1,10]
+list1.sort()
+print("the reverse list is:",list1)
+
+list3 = []
+list3 = list1.copy()
+print("list3 is:",list3)
+
+indexvalue = list2[1:4]
+print("the index values are", indexvalue)
